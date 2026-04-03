8ebba8fd173de3ae7b6304f4eb0434f73f11c276
ice21390106 <ice21390106@uniwa.gr>
2026-04-03 15:41:02 +0300

    minor fixes

--- src/cpu.c
+++ src/cpu.c
@@ -25,7 +25,7 @@ void cpu_nmi(CPU *cpu){
     stack_push(cpu, (cpu->pc & 0xFF)); // low byte
     // pushes flags to stack, forces U=1 (always 1 but forces it anyway), B=0 ONLY IN STACK
     // technically unneeded to force but 6502 does that so whatever 
-    stack_push(cpu, (cpu->status | FLAG_U) & ~FLAG_B)
+    stack_push(cpu, (cpu->status | FLAG_U) & ~FLAG_B);
 
     set_flag(cpu,FLAG_I,1); // disables further interrupts (since already in interrupt handling)
 
@@ -38,7 +38,7 @@ void cpu_nmi(CPU *cpu){
 
 // interrupt request
 void cpu_irq(CPU *cpu){
-    if get_flag(cpu,FLAG_I) return; // interrupts disabled
+    if (get_flag(cpu,FLAG_I)) return; // interrupts disabled
 
     // same as NMI
     stack_push(cpu, (cpu->pc >> 8) & 0xFF); // high byte

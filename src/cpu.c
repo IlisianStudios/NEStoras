cd768f5ea85dc362cdb5c144d5115a10245948dd
avvie <pandora.konof@gmail.combrew>
2026-04-04 15:45:47 +0300

    executing the instruction after we checked the current state the cpu

--- src/cpu.c
+++ src/cpu.c
@@ -137,11 +137,6 @@ void cpu_step(CPU *cpu) {
     Instruction inst = lookup[cpu->fetched];
 
     uint8_t extra1 = inst.addrmode(cpu);
-    uint8_t extra2 = inst.operate(cpu);
-
-    uint8_t cycles = inst.cycles + (extra1 & extra2);
-    cpu->cycles = cycles;
-    cpu->total_cycles += cycles;
 
     if (cpu->testing_mode)
         log_cpu_state(cpu, inst, pc_snapshot);  // pass snapshot
@@ -151,7 +146,11 @@ void cpu_step(CPU *cpu) {
         if (!nestest_compare(&nestest_log, cpu, &inst, pc_snapshot))
             exit(1);
     #endif
+    uint8_t extra2 = inst.operate(cpu);
 
+    uint8_t cycles = inst.cycles + (extra1 & extra2);
+    cpu->cycles = cycles;
+    cpu->total_cycles += cpu->cycles ;
 }
 
 void set_flag(CPU *cpu, const uint8_t flag, const bool value) {
